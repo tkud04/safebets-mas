ba7d73700badde1bba0e11f080b8e6a95b63fc92
tkud04 <32499735+tkud04@users.noreply.github.com>
2018-05-18 15:58:55 +0100

    Layout update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,6 +8,18 @@
 		     <div class="col-md-12">
 			     @include("input-errors")
 			 </div>
+			<div class="col-md-12">
+				<?php
+				     $msg = "";
+			       if(Session::has("reset-status") && Session::get("reset-status") == "success") $msg = "Your password change was successful";
+				?>
+				<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+               </div>
+            </div>
 		 </div>
     </div>
 </section>
