1fb4b876550e4a85f1970eeea6a369c5dc9a8a20
tkud04 <32499735+tkud04@users.noreply.github.com>
2018-05-18 15:57:39 +0100

    Password alert update

--- resources/views/emails/password_alert.blade.php
+++ resources/views/emails/password_alert.blade.php
@@ -5,7 +5,4 @@ $u = url("change-password")."/".$rxf;
 <center>
 <p>Hello, you recently requested for a new password. Please click <a href="{{$url}}">here</a> or copy and paste the link below to your browser:</p><br>
 <p><a href="{{$url}}">{{$url}}</a><br>
-<p><strong>Password: </strong>{{$pass}}</p><br>
 </center><br>
-
-<p style="font-color: red;"><em><strong>Follow up on your clients and most importantly, GET PAID! Good luck</strong></em></p><br>
