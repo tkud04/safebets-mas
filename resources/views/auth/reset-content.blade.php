7ecbb800376ce975b6a97e0fadcc8914218ace21
tkud04 <32499735+tkud04@users.noreply.github.com>
2018-05-18 15:56:06 +0100

    Reset content update

--- resources/views/auth/reset-content.blade.php
+++ resources/views/auth/reset-content.blade.php
@@ -5,9 +5,14 @@
 		   <h2 class="section-heading">Set Your New Password</h2>
             									<form action="{{url('change-password')}}" method="post">
 											   {{csrf_field()}}
+											<input type="hidden" name="rxf"/>
 												<div class="form-group">
-												    <label for="email"><strong>ENter your email address <span class="required">*</span></strong></label>
-													<input class="form-control" type="email" id="email" name="email" placeholder="Email address" value="{{old('email')}}" />
+												    <label><strong>Enter your new password<span class="required">*</span></strong></label>
+													<input class="form-control" type="password" name="password" placeholder="Your new password"/>
+												</div>
+												<div class="form-group">
+												    <label><strong>Confirm your new password<span class="required">*</span></strong></label>
+													<input class="form-control" type="password" name="password_confirmation" placeholder="Confirm password"/>
 												</div>
 												<div class="form-group">					
 													<button type="submit" class="btn btn-success">Submit</button> 
