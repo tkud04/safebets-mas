1fbb1055df117cf860f5833af03cba2ce7e4487a
tkud04 <32499735+tkud04@users.noreply.github.com>
2018-05-20 07:49:13 +0100

    Update database config

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
          //
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'heroku_139d0ae14be33da'),
             'username' => env('DB_USERNAME', 'b9297323198061'),
