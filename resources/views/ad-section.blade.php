9e2a28363625214835b607968a31135ad6bf9ec2
ANonymous <postmaster.richmama@gmail.com>
2018-06-03 20:11:31 +0100

    tidying up

--- resources/views/ad-section.blade.php
+++ resources/views/ad-section.blade.php
@@ -3,7 +3,7 @@
       <div class="container-fluid p-0">
         <div class="row no-gutters popup-gallery">
 		  @foreach($ads as $ad)
-		  <?php $img = asset($ad['img']); $href = $ad['href']; ?>
+		  <?php $img = $ad['img']; $img = asset($img); $href = $ad['href']; ?>
           <div class="col-lg-4 col-sm-6">
             <a class="portfolio-box" href="{{$href}}" target="_blank">
               <img class="img-fluid" src="{{$ad}}" alt="" width="650" height="350">
