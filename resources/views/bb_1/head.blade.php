ec392a8ae656f91bd3ccec1898649d2857185909
ANonymous <postmaster.richmama@gmail.com>
2018-06-14 23:58:10 +0100

    finishing up

--- resources/views/bb_1/head.blade.php
+++ resources/views/bb_1/head.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:image" content="{{asset('landings/bb-1/assets/img/safebets-logo.png')}}">
+    <meta property="og:title" content="Subscribe to SafeBets VIP List to Get Sure 3+ Odds and Cashout Daily!">
   
     <title>Subscribe to SafeBets VIP List to Get Sure 3+ Odds and Cashout Daily!</title>
 
