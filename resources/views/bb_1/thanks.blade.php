19cb5acebd4eb5412a36dd1e34476eb6e16ffdf3
tkud04 <32499735+tkud04@users.noreply.github.com>
2018-06-19 16:48:15 +0100

    Added thanks via web

---
+++ resources/views/bb_1/thanks.blade.php
@@ -0,0 +1,8 @@
+<div class="card">
+                            <div class="card-body">
+                              <h3 class="text-success">Congratulations! You are now subscribed to the VIP list!</h3><br><br>
+							  <p><strong><i class="fa fa-arrow-right"></i> You will receive FREE winning tips of 3+ odds and above from us DAILY. Make sure you don't let these tips go to waste!</strong></p><br>
+							  <p><strong><i class="fa fa-arrow-right"></i> Also to ensure our tips get delivered to your INBOX make sure you add our email <em>safebets@disenado.com.ng</em> to your Whitelist.</strong></p><br>
+							  <p><strong><i class="fa fa-arrow-right"></i> We respect your privacy and will NEVER spam your inbox. Also if you want to stop receiving our emails you an unsubscribe anytime by clicking Unsubscribe at the bottom of each email we send to you.</strong></p><br><br>
+                            </div>
+                        </div>
