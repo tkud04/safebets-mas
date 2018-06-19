83dc2560555782c0d77319144ba7bec3383b579c
tkud04 <32499735+tkud04@users.noreply.github.com>
2018-06-19 17:55:55 +0100

    Update cta via web

--- resources/views/bb_1/cta.blade.php
+++ resources/views/bb_1/cta.blade.php
@@ -5,7 +5,7 @@
                     <div class="col-md-12 text-center">
                         <h2 class="wow animated slideInRight" data-wow-delay=".4s"><a rel="nofollow" href="#">Drop Your Email Below to Subscribe to VIP Mailing List:</a></h2>
 						<form action ="{{url('subscribe')}}" method="post">
-							<input type="hidden" id="hh" value="{{csrf_token()}}"/>
+							<input type="hidden" id="hh" name="_token" value="{{csrf_token()}}"/>
 							<input type="hidden" id="hoh" value="{{url('subscribe')}}"/>
 						 <div class="form-group">
 						    <div class="row">
