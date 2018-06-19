d3d4d008ee62a7df59017dcb8bf002c64b1018e8
ANonymous <postmaster.richmama@gmail.com>
2018-06-19 13:04:49 +0100

    added email

--- resources/views/emails/header.blade.php
+++ resources/views/emails/header.blade.php
@@ -3,7 +3,7 @@
 <style type="text/css">
 .header{
    margin-bottom: 3px;
-   font-size: 14px;
+   font-size: 9x;
 }
 
 .gdpr{
@@ -15,7 +15,7 @@
 }
 
 .footer{
-   font-size: 14px;
+   font-size: 9px;
    background: rgba(0,0,0,0.1);
    text-align: center;
 }
@@ -24,7 +24,7 @@
 <body>
   <div class="header">
     <div class="gdpr">
-      Hi! Please add <strong>safebets@disenado.com.ng</strong> to your address book so you don't miss all the valuable tips and predictions we are sending you daily to your inbox. <a href="#">If you do not want to receive these emails, you may unsubscribe here.</a>
+      Hi! Please add <strong>safebets@disenado.com.ng</strong> to your address book and mark as NOT SPAM so you don't miss all the valuable tips and predictions we are sending you daily to your inbox. <a href="#">If you do not want to receive these emails, you may unsubscribe here.</a>
 	</div>
 	<img src="{{asset('img/logo.JPG')}}" height="150" alt="Bet with sense. Choose SafeBets!"/><br><br>
   </div>
