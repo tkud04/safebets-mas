85fd871b1fbc23c574fd12c65c9507b09a512897
ANonymous <postmaster.richmama@gmail.com>
2018-06-19 19:10:13 +0100

    added email

---
+++ resources/views/emails/thanks-1.blade.php
@@ -0,0 +1,12 @@
+@include("emails.header")
+
+  <div class="body">
+
+    <strong>Congratulations! You are now subscribed to the VIP list!</strong><br><br>
+    You will receive FREE winning tips of 3+ odds and above from us DAILY. <strong>Make sure you don't let these tips go to waste!</strong><br><br>
+    Also to ensure our tips get delivered to your INBOX make sure you add our email <em>safebets@disenado.com.ng</em> to your Whitelist<br><br>
+    We respect your privacy and will NEVER spam your inbox. Also if you want to stop receiving our emails you can unsubscribe anytime by clicking <strong>Unsubscribe</strong> at the bottom of this email.<br><br>
+   <center>Trust our tips... and CASHOUT DAILY!</center><br><br>
+  </div><br><br>
+
+ @include("emails.footer")
