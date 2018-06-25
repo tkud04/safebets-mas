0952e821d9498f28e3ee98e328bd10040429decb
ANonymous <postmaster.richmama@gmail.com>
2018-06-25 15:59:53 +0100

    show booking code in view game modal

--- public/js/lib/sweetalert/sweetalert.init.js
+++ public/js/lib/sweetalert/sweetalert.init.js
@@ -94,7 +94,8 @@ function gg(al,ct,id,tk,url){
 	   else if(ret['opstatus'] == "ok"){
 		   //$('#fixtures').html("<option value='none'>Select fixture</option>");
 		   $("#vg-id").html(ret['id']);
-		   $("#bc").html("<strong>" + ret['booking-code'] + "</strong> (" + ret['bsite'] + ")");
+		   console.log(ret);
+		   $(".bc").html("<strong>" + ret['booking-code'] + "</strong> (" + ret['bsite'] + ")");
 		   var vg = ret['matches'];
 		   console.log(vg);
 		   
