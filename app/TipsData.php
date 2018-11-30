694330e991734955625e8ff8ef7ce33a87631403
tkud04 <32499735+tkud04@users.noreply.github.com>
2018-11-30 21:53:20 +0100

    Tips models

---
+++ app/TipsData.php
@@ -0,0 +1,14 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TipsData extends Model {
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['id', 'tid', 'confidence', 'likes', 'comments'];
+
+}
