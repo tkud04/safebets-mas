c00c7f75e27efea7379f9ed0dbf018471c117af2
tkud04 <32499735+tkud04@users.noreply.github.com>
2018-12-05 14:40:24 +0100

    Guide copy

--- resources/views/guide-copy.blade.php
+++ resources/views/guide-copy.blade.php
@@ -1,70 +1,28 @@
     <section id="about" class="bg-primary">
-	 <h2 class="section-heading text-center text-white">TOKENS</h2>
+	 <h2 class="section-heading text-center text-white">SUBSCRIBE FOR PREMIUM</h2>
       <div class="container">
         <div class="row">
           <div class="col-lg-12 ml-auto text-faded" style="color:#fff !important;">
                      <br>		  
-All transactions on this platform are done using <em style="font-size:1.1em;">Tokens</em>. This make it <strong>very easy</strong> to make a purchase without worrying about slow network or online fraud.<br><br>
-<br>
-<h3 class="text-white">TIPS PRICES</h3>
-<table class="table table-responsive" style="border-collapse:collapse;">
-<thead>
-<tr style="background:#fff; color:#f05f40;">
-<th style="padding-right:15px;">Type</th>
-<th style="padding-right:15px;">Price</th>
-<th>Booking Code?</th>
-</tr>
-</thead>
-<tbody>
-<tr style="color:#fff;">
-<td style="padding-right:15px;padding-bottom:3px;"><strong>Unverified</strong></td>
-<td style="padding-right:15px;padding-bottom:3px;"><em>FREE</em></td>
-<td style="text-align:center;padding-bottom:3px;">Yes</td>
-</tr>
-<tr style="background:#efefef;color:#f05f40;padding-bottom:3px;">
-<td style="padding-right:15px;padding-bottom:3px;"><strong>Regular (single-game)</strong></td>
-<td style="padding-right:15px;padding-bottom:3px;"><em>2 Tokens</em></td>
-<td style="text-align:center;padding-bottom:3px;">Yes</td>
-</tr>
-<tr style="color:#fff;">
-<td style="padding-right:15px;padding-bottom:3px;"><strong>Regular (multi-game)</strong></td>
-<td style="padding-right:15px;padding-bottom:3px;"><em>4 Tokens</em></td>
-<td style="text-align:center;padding-bottom:3px;">Yes</td>
-</tr>
-<tr style="background:#efefef;color:#f05f40;">
-<td style="padding-right:15px;padding-bottom:3px;"><strong>Premium (single-game)</strong></td>
-<td style="padding-right:15px;padding-bottom:3px;"><em>4 Tokens</em></td>
-<td style="text-align:center;padding-bottom:3px;">Yes</td>
-</tr>
-<tr style="color:#fff;">
-<td style="padding-right:15px;padding-bottom:3px;"><strong>Premium (multi-game)</strong></td>
-<td style="padding-right:15px;padding-bottom:3px;"><em>8 Tokens</em></td>
-<td style="text-align:center;padding-bottom:3px;">Yes</td>
-</tr>
-</tbody>
-</table>
+Like our free tips? <em style="font-size:1.1em;">Try premium</em>! With premium tips you'll get only the <strong>surest predictions</strong> from our team of experts on a daily basis. What's more, if we send you a premium tip and its lost (nobody's perfect) you'll get another premium tip the next day for FREE<br><br>
 <br><br>
 	
-<h3 class="text-white">HOW TO ORDER</h3>
-Tokens are available for purchase via <strong>Token Packs</strong>. Please see below for prices.<br><br>
-Don't be left out, <strong>BUY A TOKEN PACK NOW!</strong> Best part is, if you buy any verified tips using tokens and the tip is lost, your tokens will be REFUNDED back to your account!<br><br>
-<br>
-<h3 class="text-white">TOKEN PACKS</h3>
+<h3 class="text-white">PREMIUM PACKAGES</h3>
 <table class="table table-responsive" style="border-collapse:collapse;">
 <thead>
 <tr style="background:#fff; color:#f05f40;">
-<th style="padding-right:15px;">Pack</th>
-<th>Price(&#8358;)</th>
+<th style="padding-right:15px;">Package</th>
+<th>Cost</th>
 </tr>
 </thead>
 <tbody>
 <tr style="color:#fff;">
-<td style="padding-right:15px;padding-bottom:3px;"><strong>Starter Pack (8 Tokens)</strong></td>
-<td style="padding-bottom:3px;"><em>1,000</em></td>
+<td style="padding-right:15px;padding-bottom:3px;"><strong>1 week (good for trying out premium)</strong></td>
+<td style="padding-bottom:3px;"><em>&#8358;1,500 ($5)</em></td>
 </tr>
 <tr style="background:#efefef;color:#f05f40;padding-bottom:3px;">
-<td style="padding-right:15px;padding-bottom:3px;"><strong>Jumbo Pack (40 Tokens)</strong></td>
-<td style="padding-bottom:3px;"><em>5,000</em></td>
+<td style="padding-right:15px;padding-bottom:3px;"><strong>1 month (recommended!)</strong></td>
+<td style="padding-bottom:3px;"><em>&#8358;6,000 ($20)</em></td>
 </tr>
 </tbody>
 </table>
@@ -73,7 +31,7 @@ Don't be left out, <strong>BUY A TOKEN PACK NOW!</strong> Best part is, if you b
 <span style="font-size:1.2em;">Payment methods accepted:</span>
 <ul style="list-style-type: none;">
 <li><i class="fa fa-money fa-2x" style="margin-right: 3px;"></i> <strong style="font-size:1.1em;">Bank transfer</strong></li>
-<li><i class="fa fa-money fa-2x" style="margin-right: 3px;"></i> <strong style="font-size:1.1em;">Western Union/Moneygram</strong></li>
+<li><i class="fa fa-money fa-2x" style="margin-right: 3px;"></i> <strong style="font-size:1.1em;">Skrill</strong></li>
 <li><i class="fa fa-bitcoin fa-2x" style="margin-right: 3px;"></i> <strong style="font-size:1.1em;">Bitcoin</strong></li>
 </ul><br>
 
