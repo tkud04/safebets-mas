6293c716f48e910d0b06886994970c66eb221277
tkud04 <32499735+tkud04@users.noreply.github.com>
2018-12-11 13:19:09 +0100

    New results

--- resources/views/results-content.blade.php
+++ resources/views/results-content.blade.php
@@ -5,18 +5,16 @@
                         <div class="card">
                             <div class="card-body">
                                 <h4 class="card-title">Results</h4>
-                                <h6 class="card-subtitle">Daily Tip Results</h6><br>
-								<a href="{{url('tips')}}" class="btn btn-lg btn-primary">GET WINNING TIPS</a><br>
+                                <h6 class="card-subtitle">Daily Tips Results</h6><br>
+								<a href="{{url('/')}}" class="btn btn-lg btn-primary">GET WINNING TIPS</a><br>
 								<input type="hidden" value="{{csrf_token()}}" id="tk"/>
                                 <div class="table-responsive m-t-40">
                                     <table id="myTable" class="table table-bordered table-striped">
                                        <thead>
                                             <tr>
                                                 <th>Date</th>
-                                                <th>Product</th>
                                                 <th>Category</th>
                                                 <th>Odds</th>
-                                                <th>Seller</th>
                                                 <th>Status</th>
                                                 <th>Action</th>
                                             </tr>
@@ -26,7 +24,6 @@
 											@foreach($betslips as $bs)
 										    <?php
 											    $category = $bs["category"];
-												$product = $bs["product"];
 												$gameStatus = $bs["status"];
 												$odds = $bs["odds"];
 												$gs = "fa fa-question-circle";
@@ -38,26 +35,20 @@
 												$badgeClass = "badge-info";
 												$statusClass = "badge-info";
 			
-												if($category == "regular") $badgeClass = "badge-success";
+												if($category == "free") $badgeClass = "badge-success";
 												else if($category == "premium") $badgeClass = "badge-premium";
 																								
-												$url = url('view-bs');
-												$seller = $bs["seller"];
+											
 												$id = $bs["id"];
+												$url = url('view-tip')."/".$id;
 											?>
                                             <tr>
                                                 <td>{{$bs["date"]}}</td>
-                                                <td>{{$product}}</td>
                                                 <td><span class="badge {{$badgeClass}}">{{$category}}</span></td>
                                                 <td><a href='#'>{{$odds}}</a><br></td>
-                                                <td><a href='#'>{{$seller}}</a><br></td>
                                                 <td><i class="{{$gs}}"></i></td>
 												<td>
-												@if(isset($user) && $user != null)
-												 <a class="btn btn-warning text-white" href="#" onclick="viewBS({{$id}},'<?php echo $url; ?>')">View</a>
-											    @else
-												 <a class="btn btn-warning text-white mlm" href="#">View</a>												
-											    @endif
+												 <a class="btn btn-warning text-white mlm" href="{{$url}}">View</a>												
 												</td>
                                             </tr>  
 											@endforeach
